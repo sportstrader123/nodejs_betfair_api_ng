a1850f3a8940b2cd46077e89c7ab1f6b0acbb2b5
Doctor C <sportstrader123@gmail.com>
2019-08-13 14:11:17 +0100

    2019-08-13 Change list_markets_by_competition.js to use module functions

--- example_scripts/list_markets_by_competition.js
+++ example_scripts/list_markets_by_competition.js
@@ -39,11 +39,8 @@ The code is run from the commandline and requires 5 parameters. These are
 
 "use strict"
 
-var https = require('https');
-var url = require('url'); 
-var fs = require('fs');
-
-const comp_id = 141; // US Major league football
+var bfapi = require("../betfair_api/betfairapi.js");
+var market_filters = require("../betfair_api/market_filters.js");
 
 run();
 
@@ -67,255 +64,112 @@ function run()
         console.log("Error - insufficient arguments supplied. Required arguments are:");
         print_cli_params();
         process.exit(1);
-    }    
-    let id         = cli_params[0];
-    let pw         = cli_params[1];
-    let app_key    = cli_params[2];
-    let cert_path  = cli_params[3];
-    let key_path   = cli_params[4];
+    }
+    let login_params = {};    
+    login_params.id            = cli_params[0];
+    login_params.pw            = cli_params[1];
+    login_params.app_key       = cli_params[2];
+    login_params.certfile     = cli_params[3];
+    login_params.keyfile      = cli_params[4];
     
-    login(id,pw,app_key,cert_path,key_path);
-    
+    bfapi.login(login_params,loginCallback);
 }
 
 //============================================================ 
-function parseListMarketCatResponse(data) 
+function loginCallback(login_response_params)
 {
-	// Callback for when listMarketCatalogue response is received
-    let response = {};
-    try
+    // Login callback - will be called when bfapi.login receives a response
+    // from the API or encounters an error
+    if (login_response_params.error === false)
     {
-        response = JSON.parse(data);
-    }
-    catch (ex)
-    {
-        console.error("Error parsing JSON response packet: " + ex.message);
-        console.error("Offending packet content: " + data);
-        return;
-    }
-    checkErrors(response);
-	let result = response.result;
-	let market_array_length = result.length;
-	console.log("Response contains " + market_array_length + " markets:");
-	for (let i = 0; i < market_array_length; i++) 
-	{
-		let market = {};
-		market.id = result[i].marketId;
-		market.marketName = result[i].event.name + ' - ' + result[i].marketName;
-		
-		let starttime = new Date(result[i].marketStartTime);
-		market.type = result[i].description.marketType;	
-		market.numSelections = result[i].runners.length;			
-		let market_string = (starttime.toTimeString() + " : " + market.id + " (" + market.marketName + ") : [" + market.type + "]");	
-		console.log(market_string);
-	}
-}
-
-//============================================================ 
-function getTodaysMarketsByCompetitionID(session_id, app_key) 
-{
-    // Create request options - the session token needs to be set here
-    // as well as the application key
-    let https_options = {
-        hostname: 'api.betfair.com',
-        port: 443,
-        path: '/exchange/betting/json-rpc/v1',
-        agent: new https.Agent(),
-        method: 'POST',
-        headers: {            
-            'Accept': 'application/json',
-            'Content-type' : 'application/json',
-            'X-Authentication' : session_id,
-            'Connection':'Keep-Alive',
-            'X-Application' : app_key,
-        }
-    }
-    
-    // Create the filter to get the markets we want.
-    
-    // We don't need to specify country codes OR event type IDs here because we are looking for
-    // markets associated with a specific competition ID
-    let filter = '{"filter":{"competitionIds":["' + comp_id + '"]';
-    
-    // market types of interest - MATCH_ODDS and CORRECT_SCORE
-	filter += ',"marketTypeCodes":["MATCH_ODDS","CORRECT_SCORE"],';
-	
-	// Set filter to give markets that have start times today - this requires Date objects
-	// and some manipulation
-	let date = new Date();
-	date.setUTCHours(23);
-	date.setUTCMinutes(59);
-	date.setUTCSeconds(59);
-    let end_date = date.toJSON();
-	date.setUTCHours(0);
-	date.setUTCMinutes(0);
-	date.setUTCSeconds(0);    
-    let start_date = date.toJSON();
-	filter += ('"marketStartTime":{"from":"'+ start_date + '","to":"' + end_date + '"}}');
-	
-	// Specify a maximum of 100 markets to be returned in the response and request that they 
-	// are sorted in start time order
-	filter += ',"sort":"FIRST_TO_START","maxResults":"100"';
-	
-	// Request other details about each market to be returned by the response
-	filter += ',"marketProjection":["MARKET_DESCRIPTION","RUNNER_DESCRIPTION","MARKET_START_TIME","EVENT","COMPETITION"]}';
-
-    // Create the request JSON now - this involves setting the filter and the JSON-RPC interface method
-    // - in this case we are calling the listMarketCatalogue operation.
-    let json_request = '{"jsonrpc":"2.0","method":"SportsAPING/v1.0/listMarketCatalogue", "params": ' + filter + ', "id": 1}';
-    
-    // Create a string buffer to store the response we get back
-    let response_buffer = '';
-    
-    // Create the HTTPS request now
-    let req = https.request(https_options,function (res) {
-        res.setEncoding('utf-8');
-        res.on('data', function (chunk) {
-            // Event handler for arrival of new data
-            // Append the new data to the buffer
-            response_buffer += chunk;
-        });
-        res.on('end', function() {
-            // Event handler for end of data received.
-            // When the transmission has ended we call the response
-            // parser function
-            parseListMarketCatResponse(response_buffer);
-        });
-        res.on('close', function(err) {
-            // Socket close error handler
-            console.log("Error - socket connection closed!");
-            console.log(err);
-        });    
-    });
+        console.log("Login successful!");
+               
+        // Create a market filters now to get the next 10 
+        // UK greyhound race WIN markets
+  
+        // Create the filter to get the markets we want. We ask for first 10 greyhound WIN 
+        // markets that start after the current time now.
+            
+        let comp_ids = [141];                                // US Major league football        
+        let market_types = ["MATCH_ODDS","CORRECT_SCORE"];   // correct score and match odds markets
+        const max_num_markets = 200;                         // maximum of 200 markets 
         
-    // Send Json request object
-    req.write(json_request, 'utf-8');
-    req.end();
-    req.on('error', function(e) {
-        // error handler for request
-        console.log('Problem with request: ' + e.message);
-    }); 
-            
-}
+        // Create start and end times to cover the current day (in UTC time zone)    
+        let date = new Date();
+        date.setUTCHours(23);
+        date.setUTCMinutes(59);
+        date.setUTCSeconds(59);
+        let end_date = date.toJSON();
+        date.setUTCHours(0);
+        date.setUTCMinutes(0);
+        date.setUTCSeconds(0);    
+        let start_date = date.toJSON();
+        
+        // Create market filter from the above data
+        const filter = market_filters.createMarketFilterByCompIDs(comp_ids,
+                                                                  market_types,
+                                                                  start_date,
+                                                                  end_date,
+                                                                  max_num_markets);                                
+        console.log(filter);
 
-//============================================================ 
-function login(id,pw,app_key,cert_path,key_path)
-{
-    console.log("Attempting to login...");
-    let login_endpoint = "https://identitysso-cert.betfair.com/api/certlogin";
-    
-    // Create URL object from the logon endpoint using the URL package
-    // See this link:  https://nodejs.org/api/url.html#url_url 
-    // for more details.
-    let login_options = url.parse(login_endpoint);
-
-    // We login with a HTTP POST request.
-    // Set the header and options - including application key and the 
-    // key file and cert file
-    login_options.method = 'POST';
-    login_options.port = 443;
-
-    // Set the application key within the headers according to betfair documentation
-    login_options.headers = {        
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'X-Application': app_key
-    };
-    login_options.key = fs.readFileSync(key_path);
-    login_options.cert = fs.readFileSync(cert_path);
-    login_options.agent = new https.Agent(login_options);
-
-    // Create a new https request object.
-    let req = https.request(login_options, function(res) {   
-        // Display the https request status code 
-        console.log("HTTP status code:", res.statusCode);
-            
-        // Create string to store the API response data
-        let responseData = "";
-        res.on('data', function(new_data) {
-            // New data arrived - append to existing string buffer
-            responseData += new_data;
-        });
-        res.on('end', function() {
-            // On end handler. Fires when the response has been fully received
-            try
-            {
-                // Parse the response JSON
-                let response = JSON.parse(responseData);
-                if (200 === res.statusCode)
-                {
-                    // Received HTTP status code 200. 
-                    // The response is a JSON object that contains a status
-                    // code. If login was successful, it also includes a session token.                    
-                    // See Betfair documentation on the non-interactive login for more
-                    // details
-                    if ("SUCCESS" === response.loginStatus)
-                    {
-                        // Successful logon!!                                    
-                        console.log("Successfully logged in.");
-                        // Store the session token. In this script we don't do anything with 
-                        // it but in a full application this must be sent with every 
-                        // API request we make to identify our session.
-                        let sess_id = response.sessionToken;        
-                                                                     
-                        // Make the listMarketCatalogue request now.                        
-                        getTodaysMarketsByCompetitionID(sess_id, app_key);                  
-                    }
-                    else
-                    {
-                        // Logon unsuccessful - API indicates a logon error - display it
-                        console.log("Login attempt failed. API loginStatus " + response.loginStatus);
-                    }
-                }
-                else
-                {
-                    // Login failed. Report the HTTP error status code
-                    console.error("Login failed! HTTP status code = " + res.statusCode);
-                }
-            }
-            catch (e)
-            {
-                // JSON parser error - report the reason that the JSON was not
-                // correctly parsed.
-                console.error("JSON parse error: " + e.message);
-            }
-        });
-        res.on('error', function(e) {
-            // Error with response - dump to console.
-            console.error(e);
-        });
-    });
-
-    // Create string that forms our request payload - this contains our login credentials
-    let data = 'username=' + id + '&password=' + pw;
-
-    // Post the request
-    req.end(data);
-    req.on('error', function(e) {
-        // Error with the request  - dump to console.
-        console.log('Problem with request: ' + e.message);
-    });
-}
-
-
-
-//============================================================ 
-function checkErrors(response) 
-{
-    // check for errors in response body
-    if (response.error != null) 
+        bfapi.listMarketCatalogue(login_response_params.session_id,
+                                  login_response_params.app_key,
+                                  filter,
+                                  parseListMarketCatResponse);
+    }
+    else
     {
-        // If the error object in the response contains only two fields it means that 
-        // there is no detailed message of the exception thrown from API-NG
-        if (Object.keys(response.error).length > 2) 
+        console.log(login_response_params.error_message);
+    }                                                                                                                    
+}
+
+
+//============================================================ 
+function parseListMarketCatResponse(response_params) 
+{
+    // Callback for when listMarketCatalogue response is received
+    // Input parameter response_params contains the following data:
+    //    1. response_params.error - a boolean error flag
+    //    2. response_params.error_message - string containing error details or "OK" when no error
+    //    3. response_params.data - string containing the JSON response
+    //    4. response_params.session_id - string storing session token value
+    //    5. response_params.app_key - string storing application key
+    if (response_params.error === false)
+    {
+        
+        let response = {};
+        try
         {
-            console.log("Error with request!!");
-            console.log(JSON.stringify(response, null, '\t'));
-            console.log("Exception Details: ");
-            console.log(JSON.stringify(response.error.data.APINGException, null, '\t'));
+            response = JSON.parse(response_params.data);
         }
-        // Exit program
-        console.log("Error with request! Error code: " + response.error.code + ", Message:" + response.error.message);
-        process.exit(1);
+        catch (ex)
+        {
+            console.error("Error parsing JSON response packet: " + ex.message);
+            console.error("Offending packet content: " + data);
+            return;
+        }
+        if (bfapi.validateAPIResponse(response))
+        {
+            let result = response.result;
+            let market_array_length = result.length;
+            console.log("Response contains " + market_array_length + " markets:");
+            for (let i = 0; i < market_array_length; i++) 
+            {
+                let market = {};
+                market.id = result[i].marketId;
+                market.marketName = result[i].event.name + ' - ' + result[i].marketName;
+                
+                let starttime = new Date(result[i].marketStartTime);
+                market.type = result[i].description.marketType;    
+                market.numSelections = result[i].runners.length;            
+                let market_string = (starttime.toTimeString() + " : " + market.id + " (" + market.marketName + ") : [" + market.type + "]");    
+                console.log(market_string);
+            }          
+        }
+    }
+    else
+    {
+        console.log(response_params.error_message);
     }
 }
-
